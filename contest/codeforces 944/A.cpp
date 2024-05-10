3b2dc2303cb943be069eb56b86e322b1ac6ed3f7
Prathamesh Mandiye <126768899+Prathamesh-Mandiye-0423@users.noreply.github.com>
2024-05-11 00:17:50 +0530

    Create A.cpp

---
+++ contest/codeforces 944/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+	ll t;cin>>t;
+	while(t--){
+		int x,y;cin>>x>>y;
+		cout<<min(x,y)<<' '<<max(x,y);
+		cout<<'\n';
+
+	}
+}
